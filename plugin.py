56a1cf1aae0a4791e3cd64154b70d59a20958170
tinypinglite <tinyping@protonmail.com>
2026-08-28 11:44:28 -0400

    chore(release): v0.1.5

--- plugin.py
+++ plugin.py
@@ -29,7 +29,7 @@ from .offline import Cloud115OfflineDownloadComponent
 
 PLUGIN_ID = "sakuramedia_115_provider"
 DISPLAY_NAME = "115 网盘"
-VERSION = "0.1.4"
+VERSION = "0.1.5"
 
 LIBRARY_CONFIG_FIELDS = (
     ConfigField(
