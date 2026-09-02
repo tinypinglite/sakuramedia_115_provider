610763ce3ac51e9d724375afaa53227bcfc2f90f
tinypinglite <tinyping@protonmail.com>
2026-09-02 16:57:36 -0400

    fix(release): align 115 provider version

--- plugin.py
+++ plugin.py
@@ -29,7 +29,7 @@ from .offline import Cloud115OfflineDownloadComponent
 
 PLUGIN_ID = "sakuramedia_115_provider"
 DISPLAY_NAME = "115 网盘"
-VERSION = "0.1.9"
+VERSION = "0.1.10"
 
 LIBRARY_CONFIG_FIELDS = (
     ConfigField(
